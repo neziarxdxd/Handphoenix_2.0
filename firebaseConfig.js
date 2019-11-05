62072baa739f3e617146aaaab267ca5a89322599
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2019-11-06 01:46:34 +0800

    added firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,22 @@
+
+<script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-app.js"></script>
+
+
+<script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBDDWUyH3gSkmoqmU9z53LCnUkeVC5r6DM",
+    authDomain: "handyphoenix-f3302.firebaseapp.com",
+    databaseURL: "https://handyphoenix-f3302.firebaseio.com",
+    projectId: "handyphoenix-f3302",
+    storageBucket: "handyphoenix-f3302.appspot.com",
+    messagingSenderId: "22319239072",
+    appId: "1:22319239072:web:c016aa4f7d49d4e7bfdec5",
+    measurementId: "G-BHJH4CRB3N"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
